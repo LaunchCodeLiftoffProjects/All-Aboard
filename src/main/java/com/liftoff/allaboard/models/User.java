d9d0fc43bcbdc5048320de27ab9bfc541fa87a4e
Abby Wagoner <awagoner05@icloud.com>
2020-12-15 18:06:11 -0600

    Updated User class to include address fields, email fields, getters and setters

--- src/main/java/com/liftoff/allaboard/models/User.java
+++ src/main/java/com/liftoff/allaboard/models/User.java
@@ -1,4 +1,4 @@
-package org.launchcode.javawebdevtechjobsauthentication.models;
+package com.liftoff.allaboard.models;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -21,12 +21,37 @@ public class User {
     @Size(max = 150)
     private String name;
 
-
     @NotNull
     private String username;
 
     @NotNull
     private String pwHash;
+
+    @NotBlank(message = "This field cannot be left blank!")
+    @Size(max = 50)
+    private String email;
+
+    @NotBlank(message = "This field cannot be left blank!")
+    @Size(max = 50)
+    private String addressLineOne;
+
+    @NotBlank(message = "This field cannot be left blank!")
+    @Size(max = 50)
+    private String addressLineTwo;
+
+    @NotBlank(message = "This field cannot be left blank!")
+    @Size(max = 50)
+    private String city;
+
+    @NotBlank(message = "This field cannot be left blank!")
+    @Size(max = 2)
+    private String state;
+
+
+    @NotBlank(message = "This field cannot be left blank!")
+    @Size(max = 5)
+    private String zipCode;
+
 
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
@@ -52,6 +77,54 @@ public class User {
 
     public String getUsername() {
         return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddressLineOne() {
+        return addressLineOne;
+    }
+
+    public void setAddressLineOne(String addressLineOne) {
+        this.addressLineOne = addressLineOne;
+    }
+
+    public String getAddressLineTwo() {
+        return addressLineTwo;
+    }
+
+    public void setAddressLineTwo(String addressLineTwo) {
+        this.addressLineTwo = addressLineTwo;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
     }
 
     @Override
