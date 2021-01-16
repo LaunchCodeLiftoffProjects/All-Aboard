f02c9d92ef43d2aa5cedf0c1271c81c25f5cf28f
KBarr <ksamuels6@yahoo.com>
2021-01-16 17:30:23 -0600

    fixing conflicts

--- src/main/java/com/liftoff/helloworld/HelloWorldApplication.java
+++ src/main/java/com/liftoff/helloworld/HelloWorldApplication.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 public class HelloWorldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
 
 
 }
